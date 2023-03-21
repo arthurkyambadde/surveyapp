9814282c85dee24c02828235ee80600b90c10731
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-22 02:04:38 +0300

    (debug): log type to the console to check whether the type gets to the choice component

--- src/components/choice/Choice.jsx
+++ src/components/choice/Choice.jsx
@@ -3,6 +3,8 @@ import CheckBox from "../inputs/CheckBox";
 import Radio from "../inputs/Radio";
 
 function Choice({ type, ...props }) {
+  console.log("type", type);
+
   if (type === "multi_select") {
     return <CheckBox {...props} />;
   }
