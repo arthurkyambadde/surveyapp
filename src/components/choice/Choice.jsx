9bc15988a5189e4eded602e7ba404e302bdfeb0d
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-22 13:36:13 +0300

    (refactor): remove log statement

--- src/components/choice/Choice.jsx
+++ src/components/choice/Choice.jsx
@@ -3,8 +3,6 @@ import CheckBox from "../inputs/CheckBox";
 import Radio from "../inputs/Radio";
 
 function Choice({ type, ...props }) {
-  console.log("type", type);
-
   if (type === "multi_select") {
     return <CheckBox {...props} />;
   }
