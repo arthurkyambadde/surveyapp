683231c72c6307f76236e8921d4c846894099a0d
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-22 13:39:56 +0300

    (feat): add component for the type of text

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -3,21 +3,37 @@ import { data } from "../../data/data";
 import { Introduction } from "../../screens/onboarding/Introduction/Introduction";
 import { Instructions } from "../../screens/onboarding/instructions/Instructions";
 import { VenueSelection } from "../../screens/question/venue_selection/VenueSelection";
+import AppointmentIntroduction from "../../screens/appointment/AppointmentIntroduction";
+import AppointmentScheduler from "../../screens/appointment/AppointmentScheduler";
 
 export default function MainLayout() {
   const layout = data.venueRankerLayout;
+  const questions = data.venueRankerQuestions;
 
-  return layout.map((item) => {
-    if (item.type === "introduction") {
-      return <Introduction {...item} key={item.id} />;
-    }
+  return (
+    <>
+      {layout.map((item) => {
+        if (item.type === "introduction") {
+          return <Introduction {...item} key={item.id} />;
+        }
 
-    if (item.type === "instructions") {
-      return <Instructions {...item} key={item.id} />;
-    }
+        if (item.type === "instructions") {
+          return <Instructions {...item} key={item.id} />;
+        }
 
-    if (item.type === "multi_choice_question") {
-      return <VenueSelection {...item} key={item.id} />;
-    }
-  });
+        if (item.type === "multi_choice_question") {
+          return <VenueSelection {...item} key={item.id} />;
+        }
+      })}
+      {questions.map((item) => {
+        if (item.type === "single_select_question") {
+          return <AppointmentIntroduction {...item} key={item.id} />;
+        }
+
+        if (item.type === "description") {
+          return <AppointmentScheduler {...item} key={item.id} />;
+        }
+      })}
+    </>
+  );
 }
--- src/screens/appointment/AppointmentScheduler.jsx
+++ src/screens/appointment/AppointmentScheduler.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Confirm from "../../components/buttons/Confirm";
 import { Date } from "../../components/inputs/Date";
 import { useVenue } from "../../hooks/useVenue";
+import Text from "../../components/inputs/Text";
+import { MediaViewer } from "../../components/MediaViewer/MediaViewer";
 
-export default function AppointmentScheduler() {
+export default function AppointmentScheduler(props) {
   const {
     handleDayChange,
     handleMonthChange,
@@ -11,57 +13,76 @@ export default function AppointmentScheduler() {
     day,
     month,
     year,
+    handleTextInputChange,
+    inputText,
   } = useVenue();
 
   return (
     <div>
       <section className="h-screen w-screen flex  items-center justify-center">
         <div className="w-1/2 p-16">
-          <p className="text-5xl mb-8">What date did you have in mind?</p>
-          <p className="text-4xl text-gray-700 mb-8 ">
-            If you havent decided just enter any date for this coming month
-          </p>
-          <div className="flex">
-            <div className="flex flex-col w-14 align-middle justify-center">
-              <Date
-                value={day}
-                holder="DD"
-                id={"day"}
-                handleInputChange={handleDayChange}
-                labelTag={"Day"}
-                type="day"
-              />
+          <p className="text-5xl mb-8">{props.question}</p>
+          <p className="text-4xl text-gray-700 mb-8 ">{props.subtitle}</p>
+          {props.input_type === "date" && (
+            <div className="flex">
+              (
+              <div className="flex flex-col w-14 align-middle justify-center">
+                <Date
+                  value={day}
+                  holder="DD"
+                  id={"day"}
+                  handleInputChange={handleDayChange}
+                  labelTag={"Day"}
+                  type="day"
+                />
+              </div>
+              <span className="p-4 text-xl text-primarybtn flex align-middle justify-center">
+                /
+              </span>
+              <div className="flex flex-col w-14 align-middle justify-center">
+                <Date
+                  value={month}
+                  holder="MM"
+                  id={"month"}
+                  handleInputChange={handleMonthChange}
+                  labelTag={"Month"}
+                  type="text"
+                />
+              </div>
+              <span className="p-4 text-xl text-primarybtn flex align-middle justify-center">
+                /
+              </span>
+              <div className="flex flex-col w-14 align-middle justify-center">
+                <Date
+                  value={year}
+                  holder="YYYY"
+                  id={"year"}
+                  handleInputChange={handleYearChange}
+                  labelTag={"Year"}
+                  type="year"
+                />
+              </div>
+              )
             </div>
-            <span className="p-4 text-xl text-primarybtn flex align-middle justify-center">
-              /
-            </span>
-            <div className="flex flex-col w-14 align-middle justify-center">
-              <Date
-                value={month}
-                holder="MM"
-                id={"month"}
-                handleInputChange={handleMonthChange}
-                labelTag={"Month"}
-                type="text"
+          )}
+          {props.input_type === "text" && (
+            <>
+              <Text
+                name={props.description}
+                type={props.type}
+                id={props.id}
+                value={inputText || ""}
+                handleInputChange={handleTextInputChange}
+                placeholder={props.placeHolder}
               />
-            </div>
-            <span className="p-4 text-xl text-primarybtn flex align-middle justify-center">
-              /
-            </span>
-            <div className="flex flex-col w-14 align-middle justify-center">
-              <Date
-                value={year}
-                holder="YYYY"
-                id={"year"}
-                handleInputChange={handleYearChange}
-                labelTag={"Year"}
-                type="year"
-              />
-            </div>
-          </div>
+            </>
+          )}
+
           <Confirm />
         </div>
-        <div className="w-1/2 flex align-middle justify-center"> image</div>
+        <div className="w-1/2 flex align-middle justify-center">
+          <MediaViewer imageUrl={props.link} />
+        </div>
       </section>
     </div>
   );
